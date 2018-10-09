00b76bee29b43d5aa63b65de3bfdaa8602520184
DamonFicus <captain.mzero@gmail.com>
2018-10-09 14:35:46 +0800

    push

---
+++ src/main/java/ficus/suitcase/EnumUtilDemo.java
@@ -0,0 +1,15 @@
+package ficus.suitcase;
+
+/**
+ * Created by 01333346 on 2018/10/9.
+ */
+public class EnumUtilDemo {
+    public static void main(String[] args) {
+        StatusEnum semByCode = EnumUtil.getEnumBycode(StatusEnum.class, 1);
+        System.out.println(semByCode);
+        StatusEnum semByName = EnumUtil.getEnumByName(StatusEnum.class, "WAITTING");
+        System.out.println(semByName);
+        StatusEnum semByDesc = EnumUtil.getEnumByDesc(StatusEnum.class, "结束");
+        System.out.println(semByDesc);
+    }
+}
---
+++ src/main/java/ficus/suitcase/StatusEnum.java
@@ -0,0 +1,46 @@
+package ficus.suitcase;
+
+/**
+ * Created by 01333346 on 2018/10/9.
+ */
+public enum StatusEnum implements CommonEnum {
+
+    WAITTING(0,"WAITTING","等待"), STARTED(1,"STARTED","开始"),END(2,"END","结束");
+
+    private int code;
+    private String name;
+    private String desc;
+
+    StatusEnum(int code, String name, String desc) {
+        this.code = code;
+        this.name = name;
+        this.desc = desc;
+    }
+
+    @Override
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
