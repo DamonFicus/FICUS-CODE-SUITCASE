19f0c54a97ba664d1442f5ebddd3580e834b4a9b
DamonFicus <captain.mzero@gmail.com>
2018-10-09 19:53:08 +0800

    RTTI

---
+++ src/main/java/ficus/suitcase/dynamicLoad/MemoryJavaFileManager.java
@@ -0,0 +1,116 @@
+package ficus.suitcase.dynamicLoad;
+
+import javax.tools.*;
+import java.io.*;
+import java.net.URI;
+import java.nio.CharBuffer;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 使用场景：
+ * 在运行期间，动态创建字节码并运行
+ * 常规的方式，对于一个java文件编译成.class文件并予以执行，一般JavaCompiler 直接run的方式
+ * 要求源码位于磁盘上，且编译后的字节码也是写在本地磁盘上
+ * 而要对于内存中动态生成或者是网络上获取的java文件，运行期动态创建并运行，
+ * 就要通过ForwardingJavaFileManager 的API去实现；
+ *
+ */
+public final class MemoryJavaFileManager extends ForwardingJavaFileManager {
+
+	private final static String EXT = ".java";
+	private Map<String, byte[]> classBytes;
+
+	public MemoryJavaFileManager(JavaFileManager fileManager) {
+		super(fileManager);
+		classBytes = new HashMap<String, byte[]>();
+	}
+
+	public Map<String, byte[]> getClassBytes() {
+		return classBytes;
+	}
+
+	public void close() throws IOException {
+		classBytes = new HashMap<String, byte[]>();
+	}
+
+	public void flush() throws IOException {
+	}
+
+	/**
+	 * A file object used to represent Java source coming from a string.
+	 */
+	private static class StringInputBuffer extends SimpleJavaFileObject {
+		final String code;
+
+		StringInputBuffer(String name, String code) {
+			super(toURI(name), Kind.SOURCE);
+			this.code = code;
+		}
+
+		public CharBuffer getCharContent(boolean ignoreEncodingErrors) {
+			return CharBuffer.wrap(code);
+		}
+
+		@SuppressWarnings("unused")
+		public Reader openReader() {
+			return new StringReader(code);
+		}
+	}
+
+	/**
+	 * A file object that stores Java bytecode into the classBytes map.
+	 */
+	private class ClassOutputBuffer extends SimpleJavaFileObject {
+		private String name;
+
+		ClassOutputBuffer(String name) {
+			super(toURI(name), Kind.CLASS);
+			this.name = name;
+		}
+
+		public OutputStream openOutputStream() {
+			return new FilterOutputStream(new ByteArrayOutputStream()) {
+				public void close() throws IOException {
+					out.close();
+					ByteArrayOutputStream bos = (ByteArrayOutputStream) out;
+					classBytes.put(name, bos.toByteArray());
+				}
+			};
+		}
+	}
+
+	public JavaFileObject getJavaFileForOutput(
+			Location location, String className,
+			JavaFileObject.Kind kind, FileObject sibling) throws IOException {
+		if (kind == JavaFileObject.Kind.CLASS) {
+			return new ClassOutputBuffer(className);
+		} else {
+			return super.getJavaFileForOutput(location, className, kind,
+					sibling);
+		}
+	}
+
+	static JavaFileObject makeStringSource(String name, String code) {
+		return new StringInputBuffer(name, code);
+	}
+
+	static URI toURI(String name) {
+		File file = new File(name);
+		if (file.exists()) {
+			return file.toURI();
+		} else {
+			try {
+				final StringBuilder newUri = new StringBuilder();
+				newUri.append("mfm:///");
+				newUri.append(name.replace('.', '/'));
+				if (name.endsWith(EXT))
+					newUri.replace(newUri.length() - EXT.length(),
+							newUri.length(), EXT);
+				return URI.create(newUri.toString());
+			} catch (Exception exp) {
+				return URI.create("mfm:///com/sun/script/java/java_source");
+			}
+		}
+	}
+}
---
+++ src/main/java/ficus/suitcase/dynamicLoad/MethodProxyUtil.java
@@ -0,0 +1,235 @@
+package ficus.suitcase.dynamicLoad;
+
+
+import com.caucho.hessian.client.HessianProxyFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.CollectionUtils;
+
+import java.lang.reflect.Method;
+import java.net.URLClassLoader;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/**
+ * 动态生成字节码并运行的场景实例；
+ * 从数据库中获取包，方法名，统一进行入口调用；
+ * @author DamonFicus
+ */
+public final class MethodProxyUtil {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MethodProxyUtil.class);
+	
+	private static final String APP_NAME = "debit-server";
+	
+	private static final int CONN_TIMEOUT = 10000;
+	
+	private static final int READ_TIMEOUT = 180000;
+
+	private static final String regex = "[\\w.]+:[\\w.]+";
+	
+	private static final Map<String, Map<String, Object>> serviceInfoMap = new HashMap<String, Map<String,Object>>();
+	
+	/**
+	 * 调用远端Hessian服务方法
+	 * @param hessianUrl 远端hessian服务访问地址
+	 * @param pkgName 包名
+	 * @param interfaceName 接口名
+	 * @param methodName 方法名
+	 * @param argTypes 参数类型 例如：{msg:java.lang.String, list:java.util.List}
+	 * @param argMap 参数值对照
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void invokeRemoteMethodByHessian(
+			String hessianUrl, String pkgName, String interfaceName, 
+			String methodName, String argTypes, Map<String, Object> argMap) {
+		try {
+			Map<String, Object> map = null;
+			synchronized (serviceInfoMap) {
+				if (serviceInfoMap.containsKey(hessianUrl)) {
+					map = serviceInfoMap.get(hessianUrl);
+				} else {
+					map = getRemoteInterfaceInfo(pkgName, interfaceName, methodName, argTypes);
+					serviceInfoMap.put(hessianUrl, map);
+				}
+			} 
+			Class<?> clazz = (Class<?>) map.get("clazz");
+			Map<String, Class> paramTypes = (Map<String, Class>) map.get("paramTypes");
+			Method method = (Method) map.get("method");
+			URLClassLoader classLoader = (URLClassLoader) map.get("classLoader");			
+			HessianProxyFactory factory = new HessianProxyFactory(classLoader);
+//			factory.setAppName(APP_NAME);
+//			factory.setUseConnPool(false);
+			factory.setConnectTimeout(CONN_TIMEOUT);
+			factory.setReadTimeout(READ_TIMEOUT);
+	        Object obj = factory.create(clazz, hessianUrl);
+	        invokeMethod(argMap, paramTypes, method, obj);   			
+		} catch (Exception e) {
+			logger.error("HessianProxyUtil.invokeRemoteMethodByHessian exception", e);
+		}
+	}
+	/**
+	 * 执行服务方法
+	 * @param argMap
+	 * @param paramTypes
+	 * @param method
+	 * @param obj
+	 * @throws Exception
+	 */
+	@SuppressWarnings("rawtypes")
+	private static void invokeMethod(Map<String, Object> argMap,
+			Map<String, Class> paramTypes, Method method, Object obj)
+			throws Exception {
+		if (!CollectionUtils.isEmpty(paramTypes)) {
+			Object[] args = new Object[paramTypes.size()];
+			int i = 0;
+			for (String key : paramTypes.keySet()) {
+				if (!argMap.containsKey(key)) {
+					throw new Exception(String.format("[%s] parameter invalid!!!", key));
+				}
+				args[i++] = argMap.get(key); 
+			}
+		    method.invoke(obj, args);	
+		} else {
+		    method.invoke(obj);	
+		}
+	}
+	/**
+	 * 获取远端接口及方法信息
+	 * @param pkgName
+	 * @param interfaceName
+	 * @param methodName
+	 * @param argTypes
+	 * @return
+	 * @throws Exception
+	 */
+	@SuppressWarnings("rawtypes")
+	private static Map<String, Object> getRemoteInterfaceInfo(
+			String pkgName, String interfaceName, String methodName, String argTypes) throws Exception {
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+        Map<String, String> types = parseArgTypes(argTypes);
+		Map<String, Class> paramTypes = null;
+		StringBuilder paramStrB = new StringBuilder();
+		if (types != null && types.size() > 0) {
+			paramTypes = new LinkedHashMap<String, Class>(types.size());
+			for (String key : types.keySet()) {
+				String className = types.get(key);
+				paramStrB.append(",").append(className).append(" ").append(key);
+				paramTypes.put(key, Class.forName(className));
+			}
+		}
+		if (paramStrB.length() > 0)	paramStrB.deleteCharAt(0);
+		paramStrB.insert(0, "(").append(")");		
+		String javaInterface = "package " + pkgName + ";" +
+				"public interface " + interfaceName + " {" +
+				"	public void " + methodName + paramStrB +";" + 
+				"}" +
+				"" +
+				"";
+		Map<String, byte[]> map = DynamicLoader.compileInterface(javaInterface);
+		DynamicLoader.MemoryClassLoader classLoader = new DynamicLoader.MemoryClassLoader(map);
+		Class<?> clazz = classLoader.loadClass(pkgName + "." + interfaceName);
+		Method method = null;        
+		if (paramTypes != null) {			
+        	method = clazz.getMethod(methodName, 
+        			(Class[])paramTypes.values().toArray(new Class[0]));
+        } else {
+        	method = clazz.getMethod(methodName);
+        } 		
+		resultMap.put("clazz", clazz);
+		resultMap.put("method", method);
+		resultMap.put("paramTypes", paramTypes);
+		resultMap.put("classLoader", classLoader);
+		return resultMap;
+	}
+	
+	/**
+	 * 解析参数类型 json格式字符串 {msg:java.lang.String, list:java.util.List}
+	 * @param argTypes
+	 * @return
+	 */
+	private static Map<String, String> parseArgTypes(String argTypes) {		
+		Map<String,String> result = new LinkedHashMap<String,String>();
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(argTypes);
+		while(matcher.find()) {
+			String[] groups = matcher.group().split(":");
+			result.put(groups[0].trim(), groups[1].trim());
+		}
+		return result;
+	}
+	
+	/**
+	 * 调用本地Spring服务方法
+	 * @param pkgName 包名
+	 * @param interfaceName 接口名
+	 * @param methodName 方法名
+	 * @param argTypes 参数类型 例如：{msg:java.lang.String, list:java.util.List}
+	 * @param argMap 参数值对照
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void invokeLocalMethod(String pkgName, String interfaceName, 
+			String methodName, String argTypes, Map<String, Object> argMap) {
+		try {			
+			Map<String, Object> map = null;
+			synchronized (serviceInfoMap) {
+				String localKey = new StringBuilder().append(pkgName)
+						.append(interfaceName).append(methodName).append(argTypes).toString();
+				if (serviceInfoMap.containsKey(localKey)) {
+					map = serviceInfoMap.get(localKey);
+				} else {
+					map = getLocalInterfaceInfo(pkgName, interfaceName, methodName, argTypes);
+					serviceInfoMap.put(localKey, map);
+				}
+			} 			
+			Class<?> clazz = (Class<?>) map.get("clazz");
+			Map<String, Class> paramTypes = (Map<String, Class>) map.get("paramTypes");
+			Method method = (Method) map.get("method");			
+			Object obj = SpringContextHolder.getBean(clazz);
+			invokeMethod(argMap, paramTypes, method, obj);
+		} catch (Exception e) {
+			logger.error("HessianProxyUtil.invokeLocalMethod exception", e);
+		}		
+	}
+
+	/**
+	 * 获取本地接口信息
+	 * @param pkgName
+	 * @param interfaceName
+	 * @param methodName
+	 * @param argTypes
+	 * @return
+	 * @throws Exception
+	 */
+	@SuppressWarnings("rawtypes")
+	private static Map<String, Object> getLocalInterfaceInfo(String pkgName,
+			String interfaceName, String methodName, String argTypes) throws Exception {
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		String interfaceFullName = pkgName + "." + interfaceName;
+		Class<?> clazz = Class.forName(interfaceFullName);
+        Map<String, String> types = parseArgTypes(argTypes);
+        Map<String, Class> paramTypes = null;
+		if (types != null && types.size() > 0) {
+			paramTypes = new LinkedHashMap<String, Class>(types.size());
+			for (String key : types.keySet()) {
+				String className = types.get(key);
+				paramTypes.put(key, Class.forName(className));
+			}
+		}
+		Method method = null;        
+		if (paramTypes != null) {			
+        	method = clazz.getMethod(methodName, 
+        			(Class[])paramTypes.values().toArray(new Class[0]));
+        } else {
+        	method = clazz.getMethod(methodName);
+        }		
+		resultMap.put("clazz", clazz);
+		resultMap.put("method", method);
+		resultMap.put("paramTypes", paramTypes);
+		return resultMap;
+	}
+}
