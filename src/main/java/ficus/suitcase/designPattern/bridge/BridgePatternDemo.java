a922f740d47abfc38001b44933f0444f1c91ec9a
DamonFicus <captain.mzero@gmail.com>
2018-11-02 13:47:03 +0800

    designPattern-bridge

--- src/main/java/ficus/suitcase/designPattern/bridge/BridgePatternDemo.java
+++ src/main/java/ficus/suitcase/designPattern/bridge/BridgePatternDemo.java
@@ -15,6 +15,7 @@ package ficus.suitcase.designPattern.bridge;
 public class BridgePatternDemo {
 
     public static void main(String[] args) {
+
         Shape redCircle = new Circle(100,100, 10, new RedCircle());
         Shape greenCircle = new Circle(100,100, 10, new GreenCircle());
 
